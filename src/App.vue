1836a5f8363e76c96e433dd5f3e9977b4b19be3e
dacmarcell <dacmarcell@gmail.com>
2024-10-25 01:36:46 -0300

    Refactor App.vue: Remove unnecessary container div and add TopHeader component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,32 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import TopHeader from './components/TopHeader.vue'
 </script>
 
 <template>
+  <div class="container">
+    <TopHeader />
+  </div>
   <RouterView />
 </template>
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
 
+* {
+  border-radius: var(--border-radius);
+  color: var(--text-primary-color);
+}
 body {
   font-family: 'Bebas Neue', sans-serif;
   margin: 0;
   padding: 0;
-  background-color: #f0f0f0;
-  border-radius: 4px;
+  background-color: var(--background-primary-color);
+}
+.container {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
 }
 </style>
