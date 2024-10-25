5422332a2cc6741e141ada57d16fea4530b828f1
dacmarcell <dacmarcell@gmail.com>
2024-10-25 01:36:18 -0300

    Refactor TopHeader.vue: Add navigation links and LinkedIn URL

---
+++ src/components/TopHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>IdeaPlanAI</h1>
+  <nav>
+    <router-link to="/" class="link">Chat</router-link>
+    <router-link to="/about" class="link">Sobre</router-link>
+    <a v-bind:href="linkedinURL" target="_blank">Contato</a>
+  </nav>
+  <div></div>
+</template>
+
+<style scoped>
+.link {
+  margin: 10px;
+  text-decoration: none;
+}
+</style>
+
+<script lang="ts">
+export default {
+  data() {
+    return { linkedinURL: 'https://www.linkedin.com/in/marcell-dactes/' }
+  },
+}
+</script>
