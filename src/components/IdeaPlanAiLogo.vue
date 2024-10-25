b4775ba2a235de322f28fbf6f90b74d1a466db8d
dacmarcell <dacmarcell@gmail.com>
2024-10-25 03:27:33 -0300

    Add IdeaPlanAiLogo component

---
+++ src/components/IdeaPlanAiLogo.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Idea<span class="green">P</span>lan<span class="green">AI</span></h1>
+</template>
+
+<style scoped>
+.green {
+  color: var(--background-secoundary-color);
+}
+</style>
