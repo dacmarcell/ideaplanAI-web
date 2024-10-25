495ea7ee92594fb3fe0792be2a8dfe91cc5086a9
dacmarcell <dacmarcell@gmail.com>
2024-10-24 22:49:13 -0300

    Refactor App.vue: Add global CSS styling

--- src/App.vue
+++ src/App.vue
@@ -5,3 +5,15 @@ import { RouterView } from 'vue-router'
 <template>
   <RouterView />
 </template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
+
+body {
+  font-family: 'Bebas Neue', sans-serif;
+  margin: 0;
+  padding: 0;
+  background-color: #f0f0f0;
+  border-radius: 4px;
+}
+</style>
