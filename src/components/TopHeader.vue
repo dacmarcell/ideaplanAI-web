10dc658c040d329c7ea98cbeeca3e53d4b1d898f
dacmarcell <dacmarcell@gmail.com>
2024-10-25 02:06:15 -0300

    Refactor TopHeader.vue: Update navigation links and contact button

--- src/components/TopHeader.vue
+++ src/components/TopHeader.vue
@@ -1,24 +1,63 @@
+<script setup lang="ts">
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons'
+import { useRoute } from 'vue-router'
+
+const currentRoute = useRoute()
+const linkedinURL = 'https://www.linkedin.com/in/marcell-dactes/'
+
+const routes = [
+  { name: 'Chat', path: '/' },
+  { name: 'Sobre', path: '/about' },
+]
+
+const getBtnClass = (btnPath: string) =>
+  currentRoute.path === btnPath ? 'green' : 'inactive-btn'
+</script>
+
 <template>
-  <h1>IdeaPlanAI</h1>
+  <h1>IdeaPlan<span class="green">AI</span></h1>
   <nav>
-    <router-link to="/" class="link">Chat</router-link>
-    <router-link to="/about" class="link">Sobre</router-link>
-    <a v-bind:href="linkedinURL" target="_blank">Contato</a>
+    <router-link
+      v-for="route in routes"
+      :key="route.path"
+      :to="route.path"
+      :class="getBtnClass(route.path)"
+      class="link"
+      >{{ route.name }}</router-link
+    >
+    <a :href="linkedinURL" target="_blank" class="link contact">
+      <span>Contato</span>
+      <FontAwesomeIcon :icon="faExternalLinkAlt" class="icon" />
+    </a>
   </nav>
   <div></div>
 </template>
 
 <style scoped>
+.green {
+  color: green;
+}
+.inactive-btn {
+  color: rgb(169, 169, 169);
+}
+nav {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 .link {
   margin: 10px;
   text-decoration: none;
 }
-</style>
-
-<script lang="ts">
-export default {
-  data() {
-    return { linkedinURL: 'https://www.linkedin.com/in/marcell-dactes/' }
-  },
+.contact {
+  display: flex;
+  align-items: center;
+  text-decoration: underline;
 }
-</script>
+.icon {
+  margin: 0px 0px 5px 5px;
+  width: 10px;
+  height: 10px;
+}
+</style>
